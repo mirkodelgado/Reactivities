fdf3a21e43da80a7ef78fff7548a403e4fb51998
Mirko Delgado <mirkodelgado@comcast.net>
2020-10-08 06:31:51 -0400

    end section11

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Persistence;
+
+using FluentValidation;
 
 namespace Application.Activities
 {
@@ -25,6 +28,19 @@ namespace Application.Activities
 
             public string Venue { get; set; }
 
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
+            }
         }
 
         public class Handler : IRequestHandler<Command>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Router} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-widgets/dist/css/react-widgets.css';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
+import 'semantic-ui-css/semantic.min.css'
+import ScrollToTop from './app/layout/ScrollToTop';
+import dateFnsLocalizer from 'react-widgets-date-fns';
+
+new dateFnsLocalizer();
+
+export const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={history}>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>,
   document.getElementById('root')
 );
 
